86082e838b28d32cc8bed19627a6c59408f944e4
JorgeLlanes <102987755+JorgeLlanes@users.noreply.github.com>
2022-06-26 11:42:11 -0500

    TASKTRACKERFILES

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//* Connecting to MongoDB 🥭
+mongoose.connect(
+  "mongodb+srv://Jnrlns:741203Jll1998@cluster0.uh93i.mongodb.net/test",
+  { useNewUrlParser: true },
+  { useUnifiedTopology: true }
+);
+
+app.use(express.static("public"));
+
+//* Creat a data schema
+const taskSchema = {
+  input: String,
+};
+
+const tasks = mongoose.model("tasks", taskSchema);
+
+//* GET
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "public/index.html");
+});
+
+//* CREATE w MongoDB
+app.post("/", (req, res) => {
+  let newTask = new tasks({
+    input: req.body.input,
+  });
+  newTask.save();
+  // res.redirect("/");
+});
+
+//* READ w/ MongoDB Postman
+app.get("/list", (req, res) => {
+  tasks.find(function (err, response) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send({ status: 200, tasks: response });
+    }
+  });
+});
+
+app.get("/findTasksByInput", function (req, res) {
+  const inputTasks = req.query.input;
+  tasks.find({ input: inputTasks }, function (err, response) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send({ status: 200, resultsFound: response.length, tasks: response });
+    }
+  });
+});
+
+//* UPDATE w/ MongoDB Postman
+app.put("/update", function (req, res) {
+  const id = req.query.id;
+  const newTask = req.query.input;
+  tasks.findByIdAndUpdate(id, { input: newTask }, function (err, response) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send({ status: 200, tasks: response });
+    }
+  });
+});
+
+//! DELETE
+app.delete("/deleteTask", function (req, res) {
+  const id = req.query.id;
+
+  tasks.findByIdAndDelete(id, function (err, response) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send({ status: 200, tasks: response });
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server listening on 3000");
+});
